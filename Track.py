a25434114566b363773d912d0903642642f77362
Ali Shah <s.ashah@outlook.com>
2021-05-10 09:27:12 -0400

    [Finished] Node class

--- Track.py
+++ Track.py
@@ -3,29 +3,23 @@
 class Track:
 
     def __init__(self, title):
-            self.__title = title
+            self.__title = title.title()
             self.__next_track = None
 
-    # TODO: Create a getter method for the title attribute, called get_title
     def get_title(self):
-        pass
+        return self.__title
 
-    # TODO: Create a setter method for the next_track attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
-    def set_title(self, title):
-        pass
-
-    # TODO: Create a getter method for the next_track attribute, called get_next_track
     def get_next_track(self):
-        pass
+        return self.__next_track
 
-    # TODO: Create a setter method for the next_track attribute, called set_next_track
+    def set_title(self, title):
+        self.__title = title.title()
+
     def set_next_track(self, next_title):
-        pass
+        self.__next_track = next_title.title()
 
-    # TODO: Using the __str___ dunder method, return a string of the track title.
     def __str__(self):
-        pass
+        return self.__title
 
-    # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Track Title -> Next Track Title'
     def __repr__(self):
-        pass
+        return f"{self.__title} -> {self.__next_track}"
