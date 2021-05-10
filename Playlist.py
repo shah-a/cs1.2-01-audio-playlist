6b25585d8b2890473731dbd1f578d5eec3aba85d
Ali Shah <s.ashah@outlook.com>
2021-05-10 09:55:50 -0400

    [Finished] LinkedList class

--- Playlist.py
+++ Playlist.py
@@ -1,38 +1,58 @@
-"""Linked-list class."""
+"""LinkedList class."""
 
 from Track import Track
 
 class Playlist:
     def __init__(self):
         self.__first_track = None
-
-    # TODO: Create a method called add_track that creates a Track object and adds it to the playlist. This method has one parameter called title.
+        # self.__len = 0
 
     def add_track(self, title):
-        pass
-
-    # TODO: Create a method called find_track that searches for whether a track exits in the playlist and returns its index.
-    # The method has one parameters, title, which is the title of the track to be searched for. If the track is found, return its index. Otherwise, return -1.
+        temp = Track(title)
+        temp.set_next_track(self.__first_track)
+        self.__first_track = temp
+        # self.__len += 1
 
     def find_track(self, title):
-        pass
-
-    # TODO: Create a method called remove_track that removes a track from the playlist. This method takes one parameter, title, which is the track that should be removed. 
+        current = self.__first_track
+        index = -1
+        found = False
+        while current != None:
+            index += 1
+            if current.get_title().lower() == title.lower():
+                found = True
+                break
+            current = current.get_next_track()
+        return index if found else -1
 
     def remove_track(self, title):
-        pass
-
-    # TODO: Create a method called length, which returns the number of tracks in the playlist.
+        current = self.__first_track
+        previous = None
+        while current != None:
+            if current.get_title().lower() == title.lower() and previous != None:
+                previous.set_next_track(current.get_next_track())
+                # self.__len -= 1
+                break
+            if current.get_title().lower() == title.lower() and previous == None:
+                self.__first_track = current.get_next_track()
+                # self.__len -= 1
+                break
+            previous = current
+            current = current.get_next_track()
 
     def length(self):
-        pass
-
-    # TODO: Create a method called print_tracks that prints a numbered list of the tracks in the playlist.
-
-    # Example:
-    # 1. Track Title 1
-    # 2. Track Title 2
-    # 3. Track Title 3
+        # return self.__len
+        current = self.__first_track
+        count = 0
+        while current != None:
+            count += 1
+            current = current.get_next_track()
+        return count
 
     def print_tracks(self):
-        pass
+        current = self.__first_track
+        count = 1
+        while current != None:
+            print(f"{count}. {current.get_title()}")
+            current = current.get_next_track()
+            count += 1
