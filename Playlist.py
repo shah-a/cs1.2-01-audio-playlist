447f91f9992a951caf492d3ac8aee17cb1af9230
Ali Shah <s.ashah@outlook.com>
2021-05-10 08:59:00 -0400

    [Cleanup]

--- Playlist.py
+++ Playlist.py
@@ -1,45 +1,38 @@
+"""Linked-list class."""
+
 from Track import Track
 
 class Playlist:
-  def __init__(self):
-    self.__first_track = None
+    def __init__(self):
+        self.__first_track = None
 
+    # TODO: Create a method called add_track that creates a Track object and adds it to the playlist. This method has one parameter called title.
 
-  # TODO: Create a method called add_track that creates a Track object and adds it to the playlist. This method has one parameter called title.
+    def add_track(self, title):
+        pass
 
-  def add_track(self, title):
-    pass
+    # TODO: Create a method called find_track that searches for whether a track exits in the playlist and returns its index.
+    # The method has one parameters, title, which is the title of the track to be searched for. If the track is found, return its index. Otherwise, return -1.
 
+    def find_track(self, title):
+        pass
 
+    # TODO: Create a method called remove_track that removes a track from the playlist. This method takes one parameter, title, which is the track that should be removed. 
 
-  # TODO: Create a method called find_track that searches for whether a track exits in the playlist and returns its index.
-  # The method has one parameters, title, which is the title of the track to be searched for. If the track is found, return its index. Otherwise, return -1.
+    def remove_track(self, title):
+        pass
 
-  def find_track(self, title):
-    pass
+    # TODO: Create a method called length, which returns the number of tracks in the playlist.
 
+    def length(self):
+        pass
 
-  # TODO: Create a method called remove_track that removes a track from the playlist. This method takes one parameter, title, which is the track that should be removed. 
+    # TODO: Create a method called print_tracks that prints a numbered list of the tracks in the playlist.
 
-  def remove_track(self, title):
-    pass
+    # Example:
+    # 1. Track Title 1
+    # 2. Track Title 2
+    # 3. Track Title 3
 
-
-
-  # TODO: Create a method called length, which returns the number of tracks in the playlist.
-
-  def length(self):
-    pass
-
-
-  # TODO: Create a method called print_tracks that prints a numbered list of the tracks in the playlist.
-
-  # Example:
-  # 1. Track Title 1
-  # 2. Track Title 2
-  # 3. Track Title 3
-
-  def print_tracks(self):
-    pass
-
-  
+    def print_tracks(self):
+        pass
